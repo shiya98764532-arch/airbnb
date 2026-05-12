a13e52a78952161a02e68b848496f9a54d74bae3
Shivani kumari <Shiya98764532@gmail.com>
2026-05-13 02:00:37 +0530

    Fixed deployment issues

--- cloudConfig.js
+++ cloudConfig.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'wanderlust_DEV',
-    allowerdFormats:[ "png", "jpg",'jpeg'],
+    allowedFormats:[ "png", "jpg",'jpeg'],
     },
     
   },
